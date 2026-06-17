e980e1c9a8c71ee63e51b846c053a838ba058292
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-06-17 16:39:04 +0530

    Create Basic_querys.sql

---
+++ Basic_querys.sql
@@ -0,0 +1,26 @@
+--SOME BASIC SQL COMMANDS
+
+-- For Create An Database 
+CREATE DATABASE db_name ;
+
+-- For Create An Table
+CREATE TABLE table_name ;
+
+-- For Insert Values Into The Table
+INSERT INTO table_name (column1,column2)
+VALUES (value1,value2) ;
+
+/*For Select All Columns 
+And Rows From The Table*/
+SELECT * FROM table_name;
+
+-- For Delete Records Only In Table
+TRUNCATE TABLE table_name;
+
+-- For Delete Table / Database 
+DELETE TABLE table_name;
+DELETE DATABASE database_name;
+
+
+
+

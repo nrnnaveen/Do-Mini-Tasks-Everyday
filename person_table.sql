d59d7edc7497a4490f3abbdfa97de091104a08ba
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-06-19 06:44:49 +0530

    Create person_table.sql

---
+++ person_table.sql
@@ -0,0 +1,17 @@
+--CREATING PERSON TABLE
+
+CREATE TABLE person (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  country VARCHAR(20)
+  );
+
+-- INSERT 5 Rows Of DATA'S INTO PERSON TABLE 
+  
+INSERT INTO person (name, email, country) VALUES 
+('Naveen', 'naveen@gmail.com', 'india'),
+('Nithish', 'nithish@gmail.com', 'india'),
+('Siva', 'siva@gmail.com', 'india'),
+('Kumar', 'kumar@gmail.com', 'india'),
+('Vijay', 'vijay@gmail.com', 'india');
